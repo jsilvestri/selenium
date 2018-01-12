dfe10fa3948d4a6c1d137080117c5f00108ca66b
Simon Stewart <simon.m.stewart@gmail.com>
2018-01-12 17:42:54 +0000

    cp: Add a timeout delay that waits until some unspecified time in the future

--- java/server/src/org/openqa/selenium/remote/server/scheduler/RetryDelays.java
+++ java/server/src/org/openqa/selenium/remote/server/scheduler/RetryDelays.java
@@ -21,6 +21,21 @@ public class RetryDelays {
     };
   }
 
+  public static RetryDelay upTo(Duration maximumTimeout) {
+    long remaining = maximumTimeout.toMillis();
+    RetryDelay delay = immediately();
+
+    long nextDelay = 500;
+    long maxDelay = 5000;
+    while (remaining > 0) {
+      delay = delay.orElse(retryAfter(Duration.ofMillis(nextDelay)));
+      remaining -= nextDelay;
+      nextDelay = Math.min(maxDelay, nextDelay + 500);
+    }
+
+    return delay;
+  }
+
   public static RetryDelay defaultStrategy() {
     return immediately();
   }
